db50e0b4160387a6135835a3689e7d7a3b4f1214
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2024-05-05 18:29:19 +0300

    Separate abstraction projects [2]

---
+++ Dispatcher/CodeExecutor.Dispatcher.Services/global.using.cs
@@ -0,0 +1,14 @@
+global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+
+
+global using CodeExecutor.Common.Models.Exceptions;
+
+global using DbModel = CodeExecutor.DB.Abstractions.Models;
+global using DbRepository = CodeExecutor.DB.Abstractions.Repository;
+global using Messaging = CodeExecutor.Messaging;
